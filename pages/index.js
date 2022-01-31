6c9a4f5ecd1018ee90795e6b551ff5673102dea7
dhwajsharma <dhwajsharma10@gmail.com>
2022-01-31 22:46:11 +0530

    Built Sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
 import Head from "next/head";
-import Image from "next/image";
 import Sidebar from "../components/Sidebar";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="flex">
+    <div className="">
       <Head>
         <title>Twitter Clone</title>
       </Head>
 
-      <main>
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
         <Sidebar />
       </main>
     </div>
