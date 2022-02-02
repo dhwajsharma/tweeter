ef879a375427ded546de76f6e088d204f5e3a27b
dhwajsharma <dhwajsharma10@gmail.com>
2022-02-02 10:31:40 +0530

    Built Login and Logout functionality

---
+++ components/Login.js
@@ -0,0 +1,38 @@
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+
+function Login({ providers }) {
+  return (
+    <div className="flex flex-col items-center space-y-20 pt-48">
+      <Image
+        src="https://rb.gy/ogau5a"
+        width={150}
+        height={150}
+        objectFit="contain"
+      />
+
+      <div>
+        {Object.values(providers).map((provider) => (
+          <div key={provider.name}>
+            <button
+              className="relative inline-block text-lg group"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-[#1d9bf0] group-hover:-rotate-180 ease"></span>
+                <span className="relative">Sign in</span>
+              </span>
+              <span
+                className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                data-rounded="rounded-lg"
+              ></span>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
 import Head from "next/head";
 import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
+// import Widgets from "../components/Widgets";
+import { getProviders, getSession, useSession } from "next-auth/react";
+import Login from "../components/Login";
+// import Modal from "../components/Modal";
+// import { modalState } from "../atoms/modalAtom";
+// import { useRecoilState } from "recoil";
 
-export default function Home() {
+export default function Home({ trendingResults, followResults, providers }) {
+  const { data: session } = useSession();
+
+  if (!session) return <Login providers={providers} />;
+
   return (
     <div className="">
       <Head>
@@ -15,4 +25,24 @@ export default function Home() {
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+    (res) => res.json()
+  );
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  );
+  const providers = await getProviders();
+  const session = await getSession(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
 }
